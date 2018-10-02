53f7d9afc2e69a07834b41c357f653a408685bdc
MasatoMakino <unagiinu.g@gmail.com>
2018-10-02 19:59:20 +0900

    main, 定義ファイルおよび公開クラスの範囲を修正。

--- bin/BasicButton.js
+++ bin/BasicButton.js
@@ -1,5 +1,10 @@
-export { BasicButtonEvent } from "./BasicButtonEvent";
-export { BasicClickButton } from "./BasicClickButton";
+export { BasicButtonEvent, BasicButtonEventType } from "./BasicButtonEvent";
+export {
+  BasicClickButton,
+  BasicButtonMaterialConfig,
+  BasicButtonLabelColorConfig,
+  BasicButtonState
+} from "./BasicClickButton";
 export { BasicCheckButton } from "./BasicCheckButton";
 export { BasicRadioButton } from "./BasicRadioButton";
 export { BasicRadioButtonManager } from "./BasicRadioButtonManager";
